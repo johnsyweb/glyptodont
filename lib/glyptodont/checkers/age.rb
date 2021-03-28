3fe4b76a52c6f191ca1ab20cc132c7ba846f7b6b
Pete Johns <paj+github@johnsy.com>
2021-03-28 19:29:37 +1100

    Extract #stale_todos and #oldest_age

--- lib/glyptodont/checkers/age.rb
+++ lib/glyptodont/checkers/age.rb
@@ -10,35 +10,40 @@ module Glyptodont
       end
 
       def check
-        @reportable_todos = oldest_todos
         message
       end
 
       def passed?
-        reportable_todos.empty?
+        stale_todos.empty?
       end
 
       private
 
-      attr_reader :todos, :threshold, :reportable_todos
+      attr_reader :todos, :threshold
 
       def message
         if todos.empty?
           "Nothing left to do"
         elsif passed?
-          oldest = todos.max_by { |t| t[:age] }
-          "At #{Glyptodont.pluralize(oldest[:age], "day")}, TODOs are fresh enough for now"
+          "At #{Glyptodont.pluralize(oldest_age, "day")}, TODOs are fresh enough for now"
         else
-          youngest_stale = reportable_todos.last[:age]
           "Some TODOs are too stale at more than #{Glyptodont.pluralize(youngest_stale - 1, "day")} old:\n" +
-            reportable_todos.map { |t| Glyptodont.format_todo(t) }.join("\n")
+            stale_todos.map { |t| Glyptodont.format_todo(t) }.join("\n")
         end
       end
 
-      def oldest_todos
-        todos.select { |todo| todo[:age] > threshold }
-             .sort_by { |todo| todo[:age] }
-             .reverse
+      def stale_todos
+        @stale_todos ||= todos.select { |todo| todo[:age] > threshold }
+                              .sort_by { |todo| todo[:age] }
+                              .reverse
+      end
+
+      def oldest_age
+        todos.max_by { |t| t[:age] }[:age]
+      end
+
+      def youngest_stale
+        stale_todos.last[:age]
       end
     end
   end
